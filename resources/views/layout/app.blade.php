8ab8c2ac27e7c15c7abbd0bdf5c25a69501b9412
Gusde Biyan Ambarista <gusdebiyan28@gmail.com>
2024-11-30 12:09:48 +0700

    commit

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <title>Tables - Ready Bootstrap Dashboard</title>
+    <title>@yield('title')</title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no'
         name='viewport' />
     <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
